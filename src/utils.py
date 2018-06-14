d46cd4f97f76bab37cd301a242f0769e86bf0303
Jan Bilek <j.bilek@mytaxi.com>
2018-06-14 23:34:50 +0200

    load_snippets function

--- src/utils.py
+++ src/utils.py
@@ -64,6 +64,59 @@ def unpickle(filename):
             part += 1
         return result
 
+def load_snippets(doc_size=200, word_tokenized=False):
+    ids = pickle.load(open('../res/documents_ids.pkl','rb'))
+    genres_ids = pickle.load(open('../res/genres_snippets.pkl','rb')).index
+    genres = pickle.load(open('../res/genres_snippets.pkl','rb'))
+    ids_train = pickle.load(open('../res/ids_train_1_genre.pkl','rb'))
+    ids_test = pickle.load(open('../res/ids_test_1_genre.pkl','rb'))
+
+    # Prepare labels
+    train_genres = genres.loc[ids_train]
+    y_train = train_genres.columns[np.argmax(train_genres.values,axis=1)]
+    test_genres = genres.loc[ids_test]
+    y_test = test_genres.columns[np.argmax(test_genres.values,axis=1)]
+    
+    if word_tokenized:
+        texts = unpickle('../res/documents_{}_word_tokenize_tokens.pkl'.format(snippet_length))
+    else:
+        texts = unpickle('../res/documents_{}_simple_tokens.pkl'.format(snippet_length))
+    texts = np.array(texts)
+    
+    if snippet_length == 3200:
+        ids = np.array(genres_ids)
+    else:
+        ids = np.array(ids)
+
+    assert ids.shape == texts.shape
+
+    text_id_dict = {i : text for i,text in zip(ids, texts)}
+
+    train_texts = np.array([text_id_dict[i] for i in ids_train])
+    print(train_texts.shape)
+    test_texts = np.array([text_id_dict[i] for i in ids_test])
+    print(test_texts.shape)
+    
+    return train_texts, test_texts, y_train, y_test
+
+
+def load_glove_embedding(size=50, output_pickle_file='glove_embeddings_{}.pkl'):
+    output_file = output_pickle_file.format(size)
+    if os.path.exists(output_file):
+        return pickle.load(open(output_file,'rb'))
+    else:
+        glove_vectors_file = GLOVE_VECTORS_FILE.format(size)
+        embeddings_index = {}
+        with open(glove_vectors_file, encoding='UTF-8') as f:
+            for line in f:
+                values = line.split()
+                word = values[0]
+                value = np.asarray(values[1:], dtype='float32')
+                embeddings_index[word] = value
+        pickle.dump(embeddings_index, open(output_file,'wb'))
+        return embeddings_index
+
+
 def load_catalog():
     """
     If the catalog is not stored locally at the given path,
